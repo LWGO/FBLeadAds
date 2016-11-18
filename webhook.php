2c8b7faec7714c18774188abff28e5d5c21b7eb3
Lars Osterberg <losterberg@hubspot.com>
2016-11-18 13:33:46 -0500

    new code

--- webhook.php
+++ webhook.php
@@ -1,7 +1,11 @@
 <?php
 
-echo $_REQUEST['hub_challenge'];
+$challenge = $_REQUEST['hub_challenge'];
+$verify_token = $_REQUEST['hub_verify_token'];
 
-$inputJSON = file_get_contents('php://input');
-$input = json_decode($inputJSON, true);
+if ($verify_token === 'abc123') {
+  echo $challenge;
+}
+
+$input = json_decode(file_get_contents('php://input'), true);
 error_log(print_r($input, true));
